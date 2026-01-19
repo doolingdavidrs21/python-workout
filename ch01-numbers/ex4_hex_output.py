a5fd481513e2f5aa917a88ddc86c1234ecf26289
doolingdavidrs21 <david@rs21.io>
2026-01-19 10:19:17 -0700

    updated ch01-numbers

--- ch01-numbers/ex4_hex_output.py
+++ ch01-numbers/ex4_hex_output.py
@@ -1,19 +1,18 @@
-def hex_output(hex_num: int):
+def hex_output(hex_str: str):
     """
     Docstring for hex_output
 
-    :param hex_num: takes in a hex integer (powers of 16 instead of 100)
+    :param hex_num: takes in a hex areinf (powers of 16 instead of 100)
     and returns the corresponding decimal integer, prints it to the screen
     :return: integer representation of the hexadecimal string
     """
-    hex_string = str(hex_num)
-    reversed_hex_string = hex_string[::-1]
+    reversed_hex_string = hex_str[::-1]
     decimal_value = 0
     for i, digit in enumerate(reversed_hex_string):
         if digit.isdigit():
             decimal_value += int(digit, base=16) * (16 ** i)
 
-    print(f"The decimal value of hexadecimal {hex_string} is {decimal_value}")
+    print(f"The decimal value of hexadecimal {hex_str} is {decimal_value}")
     return decimal_value
 
 
