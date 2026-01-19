bb8c14171e76d8b58045cb2c66fd28c83ef2be9a
doolingdavidrs21 <david@rs21.io>
2026-01-19 10:14:09 -0700

    updated ch01-numbers

---
+++ ch01-numbers/ex4_hex_output.py
@@ -0,0 +1,21 @@
+def hex_output(hex_num: int):
+    """
+    Docstring for hex_output
+
+    :param hex_num: takes in a hex integer (powers of 16 instead of 100)
+    and returns the corresponding decimal integer, prints it to the screen
+    :return: integer representation of the hexadecimal string
+    """
+    hex_string = str(hex_num)
+    reversed_hex_string = hex_string[::-1]
+    decimal_value = 0
+    for i, digit in enumerate(reversed_hex_string):
+        if digit.isdigit():
+            decimal_value += int(digit) * (16 ** i)
+
+    print(f"The decimal value of hexadecimal {hex_string} is {decimal_value}")
+    return decimal_value
+
+
+
+
