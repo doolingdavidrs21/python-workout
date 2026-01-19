bd50c720fe3720c5f8a2ebb28ea6fc1c0825a1b2
doolingdavidrs21 <david@rs21.io>
2026-01-19 11:12:38 -0700

    updated ch02-strings

--- ch02-strings/ex7_ubbi_dubbi.py
+++ ch02-strings/ex7_ubbi_dubbi.py
@@ -10,22 +10,6 @@ forgive this slight difference in definition.
 """
 
 
-def ubbi_dubbi_clanker(word:str):
-    """
-    Convert a given word to Ubbi Dubbi.
-    Rules:
-    - For every vowel (a, e, i, o, u) in the word, add "ub" before it.
-    """
-    word = word.lower()
-    vowels = 'aeiou'
-    ubbi_dubbi_word = ''
-    for char in word:
-        if char in vowels:
-            ubbi_dubbi_word += 'ub' + char
-        else:
-            ubbi_dubbi_word += char
-    return ubbi_dubbi_word
-
 def ubbi_dubbi(word:str):
     """
     Convert a given word to Ubbi Dubbi.
@@ -34,7 +18,12 @@ def ubbi_dubbi(word:str):
     """
     word = word.lower()
     vowels = 'aeiou'
-    word_chars = list(word)
-    new_word = ''.join([word_chars.insert(i, 'ub')
-     for i in range(len(word_chars)) if word_chars[i] in vowels])
-    return new_word
+    ubbi_dubbi_word = []
+    for char in word:
+        if char in vowels:
+            ubbi_dubbi_word.append('ub' + char)
+        else:
+            ubbi_dubbi_word.append(char)
+    return ''.join(ubbi_dubbi_word)
+
+
