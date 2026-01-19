7c60fd333d32f5c6a6b9f70748353325c5a446f3
doolingdavidrs21 <david@rs21.io>
2026-01-19 11:47:17 -0700

    updated ch03-lists-tuples

--- ch03-lists-tuples/ex9_first_last.py
+++ ch03-lists-tuples/ex9_first_last.py
@@ -8,8 +8,11 @@ firstlast('abc') will return the string ac, while
 firstlast([1,2,3,4]) will return the list [1,4].
 """
 
-def firstlast(seq):
-    if len(seq) == 0:
-        raise ValueError("The sequence must have at least one element.")
-    return type(seq)([seq[0], seq[-1]]) if isinstance(seq, (list, tuple)) else seq[0] + seq[-1]
+# def firstlast(seq):
+#     if len(seq) == 0:
+#         raise ValueError("The sequence must have at least one element.")
+#     return type(seq)([seq[0], seq[-1]]) if isinstance(seq, (list, tuple)) else seq[0] + seq[-1]
 
+
+def firstlast(seq):
+    return seq[:1] + seq[-1:]
